27095503190b5b4a597554efdeadc8cd546ad2ec
xiac2060 <40709446+xiac2060@users.noreply.github.com>
2018-07-20 16:06:20 -0400

    Update and rename dataface.py to dataface_funcs.py

--- face_recognizer/dataface_funcs.py
+++ face_recognizer/dataface_funcs.py
@@ -29,3 +29,13 @@ def dataface_add(dataface, name, descriptor, amount=0.25):
         # modify partially
         old = dataface[name]
         dataface[name] = (1-amount) * old + amount * descriptor
+
+from pickle import load, dump
+
+def pickOpen(path = 'dataface.dat'):
+    file = open(path, 'rb')
+    return load(file)
+
+def pickSave(obj, path = 'dataface.dat'):
+    file = open(path, 'wb')
+    dump(obj, file)
