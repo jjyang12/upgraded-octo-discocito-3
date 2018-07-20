3cd8578fbac68c3fd714fdf8b5917bb492631f10
HardBoiled800 <HardBoiled800@users.noreply.github.com>
2018-07-20 10:15:34 -0400

    Added the faceID code '

---
+++ faceID.py
@@ -0,0 +1,32 @@
+from matplotlib.patches import Rectangle
+from matplotlib.pyplot import text
+import pylab as plt
+
+
+def faceID(pic, detections, names):
+    '''
+    Displays the picture with boxes and names located around each face.
+    
+    PARAMETERS:
+        pic - The image array with the faces to be modified.
+        detections - The face detection data. Output of the face_detect function in list form.
+        names - The list of names associated with the faces. Indexing should correlate with detections.
+        
+    OUTPUT:
+        None.
+    '''
+    
+    assert len(detections) == len(names), 'Names list must correlate with detections list.'
+    
+    fig, ax = plt.subplots()
+    ax.imshow(pic)
+    
+    for k, d in enumerate(detections):
+        # Get the landmarks/parts for the face in box d.
+        # Draw the face landmarks on the screen.
+    
+        l, t, r, b = detections[k].left(), detections[k].top(), detections[k].right(), detections[k].bottom()
+        rect = Rectangle((l,b) , r - l, t - b, fill = False)
+        ax.add_patch(rect)
+    
+        loc = text(l + 7, b - 11, names[k], bbox = dict(facecolor='white', alpha=1))
