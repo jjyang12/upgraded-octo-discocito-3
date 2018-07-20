13329582e56c8f96e1088c612412b81b6935e66b
Christina Xiao <christinaxiao@Christinas-MacBook-Air.local>
2018-07-20 16:14:27 -0400

    fixed imports and paths

--- face_recognizer/dataface_funcs.py
+++ face_recognizer/dataface_funcs.py
@@ -32,10 +32,10 @@ def dataface_add(dataface, name, descriptor, amount=0.25):
 
 from pickle import load, dump
 
-def pickOpen(path = 'dataface.dat'):
+def pickOpen(path = '../dataface.dat'):
     file = open(path, 'rb')
     return load(file)
 
-def pickSave(obj, path = 'dataface.dat'):
+def pickSave(obj, path = '../dataface.dat'):
     file = open(path, 'wb')
     dump(obj, file)
