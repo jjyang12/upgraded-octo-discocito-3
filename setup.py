6f4dc488d1974dbe4d7dda5db0d7dcf39dee3563
xiac2060 <40709446+xiac2060@users.noreply.github.com>
2018-08-03 10:43:39 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+def do_setup():
+    setup(name='face_recognizer_and_clusterer',
+          version="0.0",
+          author='upgraded-octo-discocito-3',
+          description="it's time to FACE the truth...(EMOTIONAL) (*NOT CLICKBAIT*)",
+          license='Cog*Works',
+          platforms=['Windows', 'Linux', 'Mac OS-X', 'Unix'],
+          packages=find_packages(),
+          install_requires=['numpy>=1.12'])
+
+if __name__ == "__main__":
+    do_setup()
