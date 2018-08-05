e591fa6ae8d94b3bc41ada9440eadd26e03cdb02
xiac2060 <40709446+xiac2060@users.noreply.github.com>
2018-08-04 21:58:18 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 def do_setup():
-    setup(name='face_recognizer_and_clusterer',
+    setup(name='facerec',
           version="0.0",
           author='upgraded-octo-discocito-3',
           description="it's time to FACE the truth...(EMOTIONAL) (*NOT CLICKBAIT*)",
